1638a499e76c1b9363e393709fef5b2c2dc96d4b
Luca Vazzano <luca.vazzano@gmx.net>
2018-02-24 16:52:30 +0100

    convert to tmBundle

--- SetlXHelper.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-import webbrowser
-
-
-class OpenQuickrefCommand(sublime_plugin.TextCommand):
-	def run(self, edit=None):
-		webbrowser.open_new_tab("https://github.com/LucaVazz/SetlXQuickreference/blob/master/SetlX-Quickreference.pdf")
